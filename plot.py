028a69c40b7d751315a93d8e1f4e018bb58b70d1
Alan0423 <alansalby@gmail.com>
2025-09-10 20:39:58 +0530

    ploting graphy

--- plot.py
+++ plot.py
@@ -1,10 +1,17 @@
-#import numpy as np 
+import numpy as np 
 import matplotlib.pyplot as plt
 import pandas as pd 
 colns=["flengths","fwidth","fsize","fconc","fconcl","fasym","fm3long","fm3trans","falpha","fdist","class"]
 df=pd.read_csv("/home/alan/Desktop/workspace/space/magic+gamma+telescope/magic04.csv",names=colns)
 print(df.head())
-plt.plot(df.head("flength"))
+plt.subplot(2, 1, 1)
+plt.plot(df["flengths"],df["fsize"],'ro')
+plt.xlabel("s")
+plt.ylabel("size")
+plt.plot(df["flengths"],df["fwidth"],'ro')
+plt.xlabel("s")
+plt.ylabel("w")
 plt.title("m")
+plt.tight_layout()
 plt.show()
 
---
+++ plot1.py
@@ -0,0 +1,7 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+x=np.array([1,2,3,4,5,6])
+plt.subplot(2,2,1)
+plt.plot(x)
+plt.title("x")
+plt.show()  
